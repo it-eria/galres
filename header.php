831b62b2cf8a8fc528c24511aba99e35ad5fa7a1
it-eria <iteria.info@gmail.com>
2018-07-27 19:03:39 +0300

    test commit

--- header.php
+++ header.php
@@ -8,6 +8,14 @@
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png"/>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/html_template/build/assets/css/vendors/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/html_template/build/assets/css/vendors/aos.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/html_template/build/assets/css/vendors/slick.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/html_template/build/assets/css/vendors/slick-theme.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/html_template/build/assets/css/style.min.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700&amp;subset=cyrillic" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body>
+<?php get_template_part("partials/organisms/header"); ?>
---
+++ partials/organisms/header.php
@@ -0,0 +1,5 @@
+<header>
+    <?php get_template_part("partials/molecules/header-first-line"); ?>
+    <?php get_template_part("partials/molecules/header-center-line"); ?>
+    <?php get_template_part("partials/molecules/header-nav"); ?>
+</header>
